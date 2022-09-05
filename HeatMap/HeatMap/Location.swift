c368a978bc20910d4568d05f98e874c112542e83
NosaibahMW <nosaibah@NosaibahMWs-MacBook-Pro.local>
2022-09-06 01:29:28 +0300

    Add the answer

---
+++ HeatMap/HeatMap/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  HeatMap
+//
+//  Created by NosaibahMW on 10/02/1444 AH.
+//
+
+import Foundation
+
+
+struct Location {
+    var latitude: Double
+    var longitude: Double
+    
+}
